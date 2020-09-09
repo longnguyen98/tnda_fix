99c9f4653b8a43117fb1066446a491ccac2bf51f
longnguyen98 <60703822+longnguyen98@users.noreply.github.com>
2020-09-09 23:09:34 +0700

    PersonDetail FE and BE done

--- tnda_fix/Controllers/ExternalController.cs
+++ tnda_fix/Controllers/ExternalController.cs
@@ -25,11 +25,30 @@ namespace tnda.Controllers
         }
 
 
-        
+        public JsonResult getPersonDetail()
+        {
+            string s_id = Request.QueryString["id"];
+            int id = int.Parse(s_id);
+            tndaEntities db = new tndaEntities();
+            Person child = db.People.Find(id);
+            //
+            Family family = db.Families.Find(child.ID_Farmily);
+            //
+            Person father = db.People.Find(family.ID_Dad);
+            //            
+            Person mother = db.People.Where(p => p.ID_Farmily == family.ID && p.ID != father.ID && p.ID != child.ID).FirstOrDefault();
+            //
+            Person glv = db.People.Where(p => p.ID_Class == child.ID_Class && (p.ID_role == 1 || p.ID_role == 2)).FirstOrDefault();
+            string glv_name = glv.ChristianName + " " + glv.FirstName + " " + glv.Name;
+            //
+            var fatherJson = new { ch_name = father.ChristianName, fname = father.FirstName, name = father.Name, role = father.Role.RoleName, phone = father.Phone };
+            var motherJson = new { ch_name = mother.ChristianName, fname = mother.FirstName, name = mother.Name, role = mother.Role.RoleName, phone = mother.Phone };
+            var json = new { id = child.ID, ch_name = child.ChristianName, fname = child.FirstName, name = child.Name, grade = child.Class.Grade.GradeName, pclass = child.Class.ClassName, role = child.Role.RoleName, glv = glv_name, birth = child.Birth.Value.ToShortDateString(), address = child.Address, father = fatherJson, mother = motherJson };
+            //
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
         public JsonResult getPersonByQuery()
         {
-
-
             string query = Request.QueryString["query"];
             query = convert(query).ToUpper();
             //
@@ -37,7 +56,6 @@ namespace tnda.Controllers
             List<Person> list = new List<Person>();
             list = db.People.ToList();
             List<object> objects = new List<object>();
-
             foreach (Person p in list)
             {
                 string cname = convert(p.ChristianName).ToUpper();
@@ -47,12 +65,17 @@ namespace tnda.Controllers
                 bool cfn = (cname + " " + fname + " " + name).Trim().Equals(query);
                 bool cn = (cname + " " + name).Trim().Equals(query);
                 bool fn = (fname + " " + name).Trim().Equals(query);
-
-                if (convert(p.ChristianName.ToUpper()).Equals(query) || convert(p.FirstName.ToUpper()).Equals(query) || convert(p.Name.ToUpper()).Equals(query) || cfn || cn || fn)
+                bool role = p.ID_role == 3;
+                if (role)
                 {
-                    var ob = new { id = p.ID, ch_name = p.ChristianName, fname = p.FirstName, name = p.Name };
-                    objects.Add(ob);
+                    if (convert(p.ChristianName.ToUpper()).Equals(query) || convert(p.FirstName.ToUpper()).Equals(query) || convert(p.Name.ToUpper()).Equals(query) || cfn || cn || fn)
+                    {
+                        var ob = new { id = p.ID, ch_name = p.ChristianName, fname = p.FirstName, name = p.Name, pclass = p.Class.Grade.GradeName + " " + p.Class.ClassName, birth = p.Birth.Value.ToShortDateString() };
+                        //
+                        objects.Add(ob);
+                    }
                 }
+
             }
             return Json(objects, JsonRequestBehavior.AllowGet);
         }
