45ca613f8f8507431b8575e3109e958845ced293
longnguyen98 <60703822+longnguyen98@users.noreply.github.com>
2020-09-10 12:37:55 +0700

    optimized detail

--- tnda_fix/Controllers/ExternalController.cs
+++ tnda_fix/Controllers/ExternalController.cs
@@ -32,20 +32,31 @@ namespace tnda.Controllers
             tndaEntities db = new tndaEntities();
             Person child = db.People.Find(id);
             //
-            Family family = db.Families.Find(child.ID_Farmily);
-            //
-            Person father = db.People.Find(family.ID_Dad);
-            //            
-            Person mother = db.People.Where(p => p.ID_Farmily == family.ID && p.ID != father.ID && p.ID != child.ID).FirstOrDefault();
-            //
-            Person glv = db.People.Where(p => p.ID_Class == child.ID_Class && (p.ID_role == 1 || p.ID_role == 2)).FirstOrDefault();
-            string glv_name = glv.ChristianName + " " + glv.FirstName + " " + glv.Name;
-            //
-            var fatherJson = new { ch_name = father.ChristianName, fname = father.FirstName, name = father.Name, role = father.Role.RoleName, phone = father.Phone };
-            var motherJson = new { ch_name = mother.ChristianName, fname = mother.FirstName, name = mother.Name, role = mother.Role.RoleName, phone = mother.Phone };
-            var json = new { id = child.ID, ch_name = child.ChristianName, fname = child.FirstName, name = child.Name, grade = child.Class.Grade.GradeName, pclass = child.Class.ClassName, role = child.Role.RoleName, glv = glv_name, birth = child.Birth.Value.ToShortDateString(), address = child.Address, father = fatherJson, mother = motherJson };
-            //
-            return Json(json, JsonRequestBehavior.AllowGet);
+            if (child.ID_role == 3)
+            {
+                Family family = db.Families.Find(child.ID_Farmily);
+                //
+                Person father = db.People.Find(family.ID_Dad);
+                //            
+                Person mother = db.People.Where(p => p.ID_Farmily == family.ID && p.ID != father.ID && p.ID != child.ID).FirstOrDefault();
+                //
+                Person glv = db.People.Where(p => p.ID_Class == child.ID_Class && (p.ID_role == 1 || p.ID_role == 2)).FirstOrDefault();
+                string glv_name = glv.ChristianName + " " + glv.FirstName + " " + glv.Name;
+                //
+                var fatherJson = new { ch_name = father.ChristianName, fname = father.FirstName, name = father.Name, role = father.Role.RoleName, phone = father.Phone };
+                var motherJson = new { ch_name = mother.ChristianName, fname = mother.FirstName, name = mother.Name, role = mother.Role.RoleName, phone = mother.Phone, role_id = child.ID_role };
+                var json = new { id = child.ID, ch_name = child.ChristianName, fname = child.FirstName, name = child.Name, grade = child.Class.Grade.GradeName, pclass = child.Class.ClassName, role = child.Role.RoleName, glv = glv_name, birth = child.Birth.Value.ToShortDateString(), address = child.Address, father = fatherJson, mother = motherJson, role_id = child.ID_role,glv_id=glv.ID };
+                //
+                return Json(json, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                var json = new { id = child.ID, ch_name = child.ChristianName, fname = child.FirstName, name = child.Name, grade = child.Class.Grade.GradeName, pclass = child.Class.ClassName, role = child.Role.RoleName, birth = child.Birth.Value.ToShortDateString(), address = child.Address, phone = child.Phone,role_id=child.ID_role};
+
+
+                return Json(json, JsonRequestBehavior.AllowGet);
+            }
+
         }
         public JsonResult getPersonByQuery()
         {
@@ -65,12 +76,12 @@ namespace tnda.Controllers
                 bool cfn = (cname + " " + fname + " " + name).Trim().Equals(query);
                 bool cn = (cname + " " + name).Trim().Equals(query);
                 bool fn = (fname + " " + name).Trim().Equals(query);
-                bool role = p.ID_role == 3;
+                bool role = p.ID_role == 3 || p.ID_role == 1 || p.ID_role == 2;
                 if (role)
                 {
                     if (convert(p.ChristianName.ToUpper()).Equals(query) || convert(p.FirstName.ToUpper()).Equals(query) || convert(p.Name.ToUpper()).Equals(query) || cfn || cn || fn)
                     {
-                        var ob = new { id = p.ID, ch_name = p.ChristianName, fname = p.FirstName, name = p.Name, pclass = p.Class.Grade.GradeName + " " + p.Class.ClassName, birth = p.Birth.Value.ToShortDateString() };
+                        var ob = new { id = p.ID, ch_name = p.ChristianName, fname = p.FirstName, name = p.Name, pclass = p.Class.Grade.GradeName + " " + p.Class.ClassName, birth = p.Birth.Value.ToShortDateString(), role = p.Role.RoleName };
                         //
                         objects.Add(ob);
                     }
