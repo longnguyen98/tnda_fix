cccd2396ce0e29a2d85ec4928bfa6961a4d172f3
User <User@MAITUAN>
2020-11-22 16:58:06 +0700

    Update gvl null

--- tnda_fix/Controllers/PersonController.cs
+++ tnda_fix/Controllers/PersonController.cs
@@ -29,8 +29,8 @@ namespace tnda_fix.Controllers
                 //            
                 Person mother = db.People.Where(p => p.ID_Farmily == family.ID && p.ID != father.ID && p.ID != child.ID).FirstOrDefault();
                 //
-                Person glv = db.People.Where(p => p.ID_Class == child.ID_Class && (p.ID_role == 1 || p.ID_role == 2)).FirstOrDefault();
-                string glv_name = glv.ChristianName + " " + glv.FirstName + " " + glv.Name;
+                Person glv = db.People.Where(p => p.ID_Class == child.ID_Class && (p.ID_role == 1 || p.ID_role == 2) && p.ID_Class != null).FirstOrDefault();
+                string glv_name = glv != null ? (glv.ChristianName + " " + glv.FirstName + " " + glv.Name) : "";
                 string img = child.Image;
                 if (string.IsNullOrEmpty(img))
                 {
@@ -41,7 +41,8 @@ namespace tnda_fix.Controllers
                 //
                 var fatherJson = new { fa_id = father.ID, ch_name = father.ChristianName, fname = father.FirstName, name = father.Name, role = father.Role.RoleName, phone = father.Phone };
                 var motherJson = new { mo_id = mother.ID, ch_name = mother.ChristianName, fname = mother.FirstName, name = mother.Name, role = mother.Role.RoleName, phone = mother.Phone, role_id = child.ID_role };
-                var json = new { id = child.ID, ch_name = child.ChristianName, fname = child.FirstName, name = child.Name,  pclass = classString, role = child.Role.RoleName, glv = glv_name, id_class = child.ID_Class, birth = birthString, address = child.Address, father = fatherJson, mother = motherJson, role_id = child.ID_role, glv_id = glv.ID, img = img };
+                var glvId = glv != null ? glv.ID : 91;
+                var json = new { id = child.ID, ch_name = child.ChristianName, fname = child.FirstName, name = child.Name, pclass = classString, role = child.Role.RoleName, glv = glv_name, id_class = child.ID_Class, birth = birthString, address = child.Address, father = fatherJson, mother = motherJson, role_id = child.ID_role, glv_id = glvId, img = img };
                 //
                 return Json(json, JsonRequestBehavior.AllowGet);
             }
@@ -54,7 +55,7 @@ namespace tnda_fix.Controllers
                 }
                 string birthString = child.Birth != null ? child.Birth.Value.ToString("dd.MM.yyy") : "";
                 string classString = child.Class != null ? (child.Class.Grade.GradeName + " " + child.Class.ClassName) : "";
-                var json = new { id = child.ID, ch_name = child.ChristianName, fname = child.FirstName, name = child.Name,  pclass = classString, id_class = child.ID_Class, role = child.Role.RoleName, birth = birthString, address = child.Address, phone = child.Phone, role_id = child.ID_role, img = image };
+                var json = new { id = child.ID, ch_name = child.ChristianName, fname = child.FirstName, name = child.Name, pclass = classString, id_class = child.ID_Class, role = child.Role.RoleName, birth = birthString, address = child.Address, phone = child.Phone, role_id = child.ID_role, img = image };
                 return Json(json, JsonRequestBehavior.AllowGet);
             }
 
